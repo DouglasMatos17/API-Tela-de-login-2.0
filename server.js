a3d5246ba8cca08e12ac4e1b2788b804d5dfd446
Douglas Matos <matos.douglas.17@gmail.com>
2024-05-29 05:06:41 -0300

    teste

---
+++ server.js
@@ -0,0 +1,8 @@
+import app from './src/app.js'
+
+const port = 3000
+
+// Escutando a porta 3000 
+app.listen(port, ()=> {
+    console.log(`Rodando o servidor http://localhost:${port}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
-const port = 3000
 
 // Criando rota padrão
 app.get('/', (req, res) => {
     res.send('ola mundo')
 })
 
-// Escutando a porta 3000 
-app.listen(port, ()=> {
-    console.log(`Rodando o servidor http://localhost:${port}`)
-})
+export default app
