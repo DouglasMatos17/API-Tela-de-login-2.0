1b39c910a2e3f25105ba55254117840c6ea626f3
Douglas Matos <matos.douglas.17@gmail.com>
2024-05-30 04:07:50 -0300

    Buscar e listar seleções por id

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ import express from 'express'
 
 const app = express()
 
+// Indicar para o express ler body como Json
+app.use(express.json())
+
 // Mock 
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G' },
     {id: 2, selecao: 'Suíça', grupo: 'G' },
-    {id: 3, selecao: 'Sérvia', grupo: 'G' },
-    {id: 4, selecao: 'Camarões', grupo: 'G' },
+    {id: 3, selecao: 'Camarões', grupo: 'G' },
+    {id: 4, selecao: 'Sérvia', grupo: 'G' }
 ]
+
+function buscarSelecaoPorID(id) {
+    return selecoes.filter( selecao => selecao.id == id)
+}
 
 // Criando rota padrão
 app.get('/', (req, res) => {
@@ -18,6 +25,11 @@ app.get('/', (req, res) => {
 // Primeiro end-point
 app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
+})
+
+// Retorna objeto por ID
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorID(req.params.id))
 })
 
 // Primeiro post, esta cadastrando uma nova seleção no ultimo indice do array selecoes.
